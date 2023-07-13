981ce8c6e049ae56967d9613deb8da4fbf8b71a3
lLija <ilija.trokovski@gmail.com>
2023-07-14 01:57:42 +0200

    Add project files.

---
+++ OOP/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Car
+    { 
+        private string _name;
+        private int _hp;
+        private string _color;
+
+        // Constructor
+         public Car(string name, int hp = 0, string color = "black")
+        {
+            _name = name;
+            Console.WriteLine(name + " was created.");
+            _hp = hp;
+            _color = color;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine(
+               _name + " is driving");
+        }
+
+        public void Stop() {
+            Console.WriteLine(_name + " is stopped");
+        }
+
+        public void Details() {
+            Console.WriteLine("The " +_color + " car " + _name + " has " + _hp + " horse power.");
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using OOP;
+
+Car audi = new Car("Audi A4",250,"red");
+audi.Drive();
+audi.Details();
+Car bmw = new Car("BMW M5",300);
+bmw.Drive();
+bmw.Details();
+
+Console.WriteLine("Press 1 to stop the car");
+string userInput = Console.ReadLine();
+
+if(userInput == "1")
+{
+    audi.Stop();
+
+}else if(userInput == "2")
+{
+    bmw.Stop();
+}else
+{
+    Console.WriteLine("Cars drives indefinetely");
+}
+
+Console.ReadLine();
